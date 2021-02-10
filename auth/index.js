1b70b58d916536f7dee628206219e599037d849c
platproit <jdtyler@platprodiag.com>
2021-02-10 06:44:49 -0600

    .

--- auth/index.js
+++ auth/index.js
@@ -1,22 +1,38 @@
-// express-oauth is a Google-provided, open-source package that helps automate
-// the authorization process.
-import Auth from '@google-cloud/express-oauth2-handlers';
+const { GoogleAuth } = require("google-auth-library");
+
+/**
+ * Instead of specifying the type of client you'd like to use (JWT, OAuth2, etc)
+ * this library will automatically choose the right client based on the environment.
+ */
+async function main() {
+  const Auth = new GoogleAuth({
+    scopes: "https://www.googleapis.com/auth/cloud-platform",
+  });
+  const client = await Auth.getClient();
+  const projectId = await Auth.getProjectId();
+  const url = `https://gmail.googleapis.com/gmail/v1/projects/${projectId}`;
+  const res = await client.request({ url });
+  console.log(res.data);
+}
+
+main().catch(console.error);
+
 // googleapis is the official Google Node.js client library for a number of
 // Google APIs, including Gmail.
-import { google } from 'googleapis';
-const gmail = google.gmail('v1');
+import { google } from "googleapis";
+const gmail = google.gmail("v1");
 
 // Specify the access scopes required. If authorized, Google will grant your
 // registered OAuth client access to your profile, email address, and data in
 // your Gmail and Google Sheets.
 const requiredScopes = [
-  'profile',
-  'email',
-  'https://www.googleapis.com/auth/gmail.modify',
-  'https://www.googleapis.com/auth/spreadsheets'
+  "profile",
+  "email",
+  "https://www.googleapis.com/auth/gmail.modify",
+  "https://www.googleapis.com/auth/spreadsheets",
 ];
 
-const auth = Auth('datastore', requiredScopes, 'email', true);
+const auth = Auth("datastore", requiredScopes, "email", true);
 
 const GCP_PROJECT = process.env.GCP_PROJECT;
 const PUBSUB_TOPIC = process.env.PUBSUB_TOPIC;
@@ -28,9 +44,9 @@ const setUpGmailPushNotifications = (email, pubsubTopic) => {
   return gmail.users.watch({
     userId: email,
     requestBody: {
-      labelIds: ['INBOX'],
-      topicName: `projects/${GCP_PROJECT}/topics/${pubsubTopic}`
-    }
+      labelIds: ["INBOX"],
+      topicName: `projects/${GCP_PROJECT}/topics/${pubsubTopic}`,
+    },
   });
 };
 
@@ -43,7 +59,7 @@ const onSuccess = async (req, res) => {
     // Set up the googleapis library to use the returned tokens.
     email = await auth.auth.authedUser.getUserId(req, res);
     const OAuth2Client = await auth.auth.authedUser.getClient(req, res, email);
-    google.options({auth: OAuth2Client});
+    google.options({ auth: OAuth2Client });
   } catch (err) {
     console.log(err);
     throw err;
@@ -53,7 +69,11 @@ const onSuccess = async (req, res) => {
     await setUpGmailPushNotifications(email, PUBSUB_TOPIC);
   } catch (err) {
     console.log(err);
-    if (!err.toString().includes('one user push notification client allowed per developer')) {
+    if (
+      !err
+        .toString()
+        .includes("one user push notification client allowed per developer")
+    ) {
       throw err;
     }
   }
